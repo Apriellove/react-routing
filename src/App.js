ed100234176117b0f3b559949e6994dfc3f3b11a
Apriellove <125114060+Apriellove@users.noreply.github.com>
2023-02-11 15:01:15 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const helloWorld = "Hello World"
+return {
+<div style={{fontSize: '50px'}}
+className="text-lg"
+>
+{helloWorld}
+Colors
+<ul>
+  <li.Red</li>
+  <li>
+}
+
+  ;
+}
+
+export default App;
